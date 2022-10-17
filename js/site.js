a7094c082ef119f70d2b9f5fa71834f754884971
andymonk8 <andymonk8@gmail.com>
2022-10-17 11:15:41 -0500

    Edit Index?! Etc.?!

--- js/site.js
+++ js/site.js
@@ -23,40 +23,6 @@ function getValues() {
 
 //-----------------------------------------------
 
-/// Setting Payments in Array?!
-
-function getPayments(amount, rate, term, monthlyPayment) {
-  let payments = [];
-  let balance = amount;
-  let totalInterest = 0;
-  let monthlyPrincipal = 0;
-  let monthlyInterest = 0;
-
-  /// Start; End; Increment?!
-  for (let month = 1; month <= term; month++) {
-    monthlyInterest = calcInterest(balance, rate);
-    totalInterest += monthlyInterest;
-    monthlyPrincipal = monthlyPayment - monthlyInterest;
-    balance = Math.abs(balance - monthlyPrincipal);
-
-    // Add Details of Table?!
-    let currentPayment = {
-      month: month,
-      monthlyPayment: monthlyPayment,
-      principal: monthlyPrincipal,
-      interest: monthlyInterest,
-      totalInterest: totalInterest,
-      balance: balance,
-    };
-
-    payments.push(currentPayment);
-  }
-
-  return payments;
-}
-
-//-----------------------------------------------
-
 /// CALCULATE PAYMENT FOR LOAN?!
 function calcPayment(amount, rate, term) {
   return (amount * rate) / (1 - Math.pow(1 + rate, -term));
@@ -78,6 +44,43 @@ function calcRate(rate) {
 
 //-----------------------------------------------
 
+/// Calculated the Stuff First?!
+/// Setting Payments in Array?!
+/// Do Math?! Etc.?!
+
+function getPayments(amount, rate, term, monthlyPayment) {
+  let payments = [];
+  let balance = amount;
+  let totalInterest = 0;
+  let monthlyPrincipal = 0;
+  let monthlyInterest = 0;
+
+  /// Start; End; Increment?!
+  for (let month = 1; month <= term; month++) {
+    monthlyInterest = calcInterest(balance, rate);
+    totalInterest += monthlyInterest;
+    monthlyPrincipal = monthlyPayment - monthlyInterest;
+    balance = Math.abs(balance - monthlyPrincipal);
+
+    // Add Details of Table?! Data?! Etc.?!
+    // Object?! Properties / Property?! Etc.?!
+    let currentPayment = {
+      month: month,
+      monthlyPayment: monthlyPayment,
+      principal: monthlyPrincipal,
+      interest: monthlyInterest,
+      totalInterest: totalInterest,
+      balance: balance,
+    };
+
+    payments.push(currentPayment);
+  }
+
+  return payments;
+}
+
+//-----------------------------------------------
+
 // DISPLAY DATA?! TABLE / TABLE DATA?!
 function displayData(payments, loanAmount, payment) {
   let tableBody = document.getElementById("tableBody");
@@ -89,25 +92,33 @@ function displayData(payments, loanAmount, payment) {
 
   // Start; End; Incrementation?!
   for (let index = 0; index < payments.length; index++) {
-    ///MY TABLE
+    /// MY TABLE
     let payRow = template.content.cloneNode(true);
 
     let payCols = payRow.querySelectorAll("td");
 
+    // Table Month # Number?!
     payCols[0].textContent = payments[index].month;
 
+    // Table Payment?!
     payCols[1].textContent = payment.toLocaleString("en-us", {
       style: "currency",
       currency: "USD",
     });
+
+    // Table Principal?!
     payCols[2].textContent = payments[index].principal.toLocaleString("en-us", {
       style: "currency",
       currency: "USD",
     });
+
+    // Table Interest?!
     payCols[3].textContent = payments[index].interest.toLocaleString("en-us", {
       style: "currency",
       currency: "USD",
     });
+
+    // Table Total Interest?!
     payCols[4].textContent = payments[index].totalInterest.toLocaleString(
       "en-us",
       {
@@ -115,11 +126,14 @@ function displayData(payments, loanAmount, payment) {
         currency: "USD",
       }
     );
+
+    // Table Balance?!
     payCols[5].textContent = payments[index].balance.toLocaleString("en-us", {
       style: "currency",
       currency: "USD",
     });
 
+    // Returns Node?! For Table?! Etc.?!
     tableBody.appendChild(payRow);
   }
 
@@ -133,24 +147,28 @@ function displayData(payments, loanAmount, payment) {
   // Total Principal is Loan Amount
   let totalPrincipalBold = Number(loanAmount);
 
+  // Display Total Principal Bold?!
   let labelPrincipalBold = document.getElementById("totalPrincipalBold");
   labelPrincipalBold.innerHTML = totalPrincipalBold.toLocaleString("en-US", {
     style: "currency",
     currency: "USD",
   });
 
+  // Display Total Interest Bold?!
   let labelInterest = document.getElementById("totalInterestBold");
   labelInterest.innerHTML = Number(totalInterestBold).toLocaleString("en-US", {
     style: "currency",
     currency: "USD",
   });
 
+  // Display Payment Bold?! "Your Monthly Payments:"?!
   let labelPaymentBold = document.getElementById("paymentBold");
   labelPaymentBold.innerHTML = Number(payment).toLocaleString("en-US", {
     style: "currency",
     currency: "USD",
   });
 
+  // DIsplay Total Cost Bold?!
   let labelTotalCostBold = document.getElementById("totalCostBold");
   labelTotalCostBold.innerHTML = Number(totalCostBold).toLocaleString("en-US", {
     style: "currency",
